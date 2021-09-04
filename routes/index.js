600ac4ae6986b920237a3d9edcf94dfafe0ec7e8
Josue <josue_alguz@hotmail.com>
2021-09-04 16:14:03 -0500

    Se agregan los routes

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,10 @@
 const router = require('express').Router();
+
+router.get('/', (req, res)=>{
+  res.send('welcome to DeiveryApp api');
+});
+
+router.use('/usuarios', require('./usuarios'));
+router.use('/mascotas', require('./mascotas'));
+
+module.exports = router;
---
+++ routes/recetas.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+
+const{
+    crearReceta,
+    obtenerReceta,
+    modificarReceta,
+    eliminarReceta
+} = require('../controllers/recetas');
+
+router.get('/',obtenerReceta);
+router.post('/',crearReceta);
+router.put('/:id', modificarReceta);
+router.delete('/:id', eliminarReceta);
+
+module.exports = router;
